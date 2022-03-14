65b63a2b89ef199d3d4d98bfa52eaf1ddc1ae4d2
Eldhose Skaria <54096387+eldhose06@users.noreply.github.com>
2022-03-14 14:10:41 +0530

    Update mock_api.js

--- mock_api.js
+++ mock_api.js
@@ -1,104 +1,107 @@
- function getReligions() {
-    return [
-        {
-            "id": 1,
-            "religion": "Christian"
-        },
-        {
-            "id": 2,
-            "religion": "Hindu"
-        },
-        {
-            "id": 3,
-            "religion": "Muslims"
-        }
-    ]
-}
- function getCaste(religion_id) {
-    let finalData = [];
-    let casteList = [
-        {
-            "id": 1,
-            "religion": 1,
-            "caste": "Jacobite"
-        },
-        {
-            "id": 2,
-            "religion": 1,
-            "caste": "Catholic"
-        },
-        {
-            "id": 3,
-            "religion": 2,
-            "caste": "Ezhava"
-        }
-    ]
-    $.each(casteList, function(i, e) {
-        if(e.religion == religion_id) {
-            finalData.push(e);
-        }
-    });
-    return finalData;
-}
- function getCategory() {
-    return 	[
-        {
-            "id": 1,
-            "category": "General"
-        },
-        {
-            "id": 2,
-            "category": "SC/ST"
-        }
-    ]
-}
- function getNationality() {
+//  function getReligions() {
+//     return [
+//         {
+//             "id": 1,
+//             "religion": "Christian"
+//         },
+//         {
+//             "id": 2,
+//             "religion": "Hindu"
+//         },
+//         {
+//             "id": 3,
+//             "religion": "Muslims"
+//         }
+//     ]
+// }
+//  function getCaste(religion_id) {
+//     let finalData = [];
+//     let casteList = [
+//         {
+//             "id": 1,
+//             "religion": 1,
+//             "caste": "Jacobite"
+//         },
+//         {
+//             "id": 2,
+//             "religion": 1,
+//             "caste": "Catholic"
+//         },
+//         {
+//             "id": 3,
+//             "religion": 2,
+//             "caste": "Ezhava"
+//         }
+//     ]
+//     $.each(casteList, function(i, e) {
+//         if(e.religion == religion_id) {
+//             finalData.push(e);
+//         }
+//     });
+//     return finalData;
+// }
+//  function getCategory() {
+//     return 	[
+//         {
+//             "id": 1,
+//             "category": "General"
+//         },
+//         {
+//             "id": 2,
+//             "category": "SC/ST"
+//         }
+//     ]
+// }
+//  function getNationality() {
 
-    /*AJAX Sample Code*/
-    /*
-    let nationality;
-    $.ajax({
-        url : "api - url",
-        dataType : "JSON",
-        type : "POST",
-        data : {religion_id: religion_id}// Only applicable for caste,
-        success : function(response) {
-            nationality = response;
-        }
-    });
-    return nationality;
-    */
-    /*AJAX Sample Code */
-    return [
-        {
-            "id": 1,
-            "nationality": "Indian"
-        },
-        {
-            "id": 2,
-            "nationality": "Others"
-        }
-    ]
-}
- function getWeightage() {
-    return [
-        {
-            "id": 1,
-            "weightageName": "Ncc",
-            "weightageDesc": "ncc certificate"
-        },
-        {
-            "id": 2,
-            "weightageName": "nss",
-            "weightageDesc": "nss certificate"
-        },
-        {
-            "id": 3,
-            "weightageName": "differently abled",
-            "weightageDesc": "differently abled"
-        }
-    ]
-}
+//     /*AJAX Sample Code*/
+//     /*
+//     let nationality;
+//     $.ajax({
+//         url : "api - url",
+//         dataType : "JSON",
+//         type : "POST",
+//         data : {religion_id: religion_id}// Only applicable for caste,
+//         success : function(response) {
+//             nationality = response;
+//         }
+//     });
+//     return nationality;
+//     */
+//     /*AJAX Sample Code */
+//     return [
+//         {
+//             "id": 1,
+//             "nationality": "Indian"
+//         },
+//         {
+//             "id": 2,
+//             "nationality": "Others"
+//         }
+//     ]
+// }
+// function getUsers(user_id) {
+//     console.log(user_id);
+//    let UserResult;
+//    return new Promise((resolve, reject) => {
+//    $.ajax({
+//        url : "http://localhost:8000/user/?id="+ user_id + "",
+//        dataType : "JSON",
+//        // contentType: "application/json",
+//        type : "GET",
+//        // data : {"religion": religion_id},// Only applicable for caste,
+//        success : function(response) {
+//            console.log(response);
+//            UserResult = response.user;
+//            resolve(castResult);
+//        },
+//        error: function (error) {
+//            alert("error   "+error)
+//            reject(error)
+//        },
+//    })
+// })
+// }
 function getReligions() {
     let religionResult;
     return new Promise((resolve, reject) => {
